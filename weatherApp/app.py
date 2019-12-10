10df722aaf4bcaafeea333cfee71356cc05fd3dd
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2019-12-10 21:30:46 +0530

    Search by city name

--- weatherApp/app.py
+++ weatherApp/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask import request
 import requests
 
 app = Flask(__name__)
@@ -7,14 +8,18 @@ app = Flask(__name__)
 @app.route('/')
 def search():
     return render_template('index.html')
-
-@app.route('/get_weather/')
+@app.route('/', methods=['POST', 'GET'])
 def get_weather():
-    url= 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=78c2cd272dcb82ff19017de3db518bdf'
-    City='Mumbai'
-    r=requests.get(url.format(City)).json()
-    return (r)
-
-
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=78c2cd272dcb82ff19017de3db518bdf'
+    City=request.form['enter_city']
+    r = requests.get(url.format(City)).json()
+    weather={
+        'City':r['name'],
+        "temperature":r['main']['temp'],
+        "description":r['weather'][0]['description'],
+        "icon":r['weather'][0]['icon'],
+        "Country":r['sys']['country']
+    }
+    return render_template('weather.html',weather=weather)
 if __name__ == "__main__":
     app.run(debug=True)
