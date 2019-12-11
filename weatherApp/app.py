f12e79e5651cb0e6950ffb6b46679023eb3fc4da
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2019-12-11 20:14:48 +0530

    Update app.py

--- weatherApp/app.py
+++ weatherApp/app.py
@@ -10,16 +10,20 @@ def search():
     return render_template('index.html')
 @app.route('/', methods=['POST', 'GET'])
 def get_weather():
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=78c2cd272dcb82ff19017de3db518bdf'
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=78c2cd272dcb82ff19017de3db518bdf'
     City=request.form['enter_city']
-    r = requests.get(url.format(City)).json()
-    weather={
-        'City':r['name'],
-        "temperature":r['main']['temp'],
-        "description":r['weather'][0]['description'],
-        "icon":r['weather'][0]['icon'],
-        "Country":r['sys']['country']
-    }
-    return render_template('weather.html',weather=weather)
+    if City !="":
+        r = requests.get(url.format(City)).json()
+        weather=\
+            {
+            'City':r['name'],
+            "temperature":r['main']['temp'],
+            "description":r['weather'][0]['description'],
+            "icon":r['weather'][0]['icon'],
+            "Country":r['sys']['country']
+            }
+        return render_template('weather.html',weather=weather)
+    else:
+        return render_template('Error.html')
 if __name__ == "__main__":
     app.run(debug=True)
