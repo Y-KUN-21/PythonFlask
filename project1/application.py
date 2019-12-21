9b7a0986d9bc6c47c88c736896c5a7d720faacb1
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2019-12-21 20:46:15 +0530

    Update application.py

--- project1/application.py
+++ project1/application.py
@@ -1,27 +1,31 @@
-from flask import Flask,render_template,url_for
+from flask import Flask, render_template, url_for, redirect,flash
+from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-
-
-
+from form import signupForm,loginForm
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 Bootstrap(app)
+app.config['SECRET_KEY'] = 'PFzS9qWgQqTUv6BQjQlmJg'
 
 @app.route('/')
 def home():
     return render_template("Home.html")
 
-@app.route('/login')
-def login():
-    return render_template("login.html")
 
-@app.route('/signup')
-def signup():
-    return render_template("signup.html")
+@app.route('/login', methods=['POST','GET'])
+def loginpage():
+    form = loginForm()
+    return render_template("login.html",title="Login",form=form)
 
 
+@app.route('/signup', methods=['POST','GET'])
+def register():
+    form = signupForm()
+    if form.validate_on_submit():
+        flash("Account created.",'danger')
+        return redirect(url_for('home'))
+    return render_template("signup.html", title="SignUp", form=form)
 
-
-
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(debug=True)
