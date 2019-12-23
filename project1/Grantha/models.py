03b610a483a6e656e8ba1b210a0e003d96d5569a
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2019-12-23 18:32:53 +0530

    Update models.py

--- project1/Grantha/models.py
+++ project1/Grantha/models.py
@@ -1,10 +1,18 @@
-from Grantha import db
+from Grantha import db, login_manager
+from flask_login import UserMixin
 
-class User(db.Model):
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20),unique=True, nullable=False)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(20), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return f"User('{self.username},{self.email}')"
+
