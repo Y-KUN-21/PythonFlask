782d7d62a5d2119720c3ddea20fffd2b65b47ab3
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2019-12-23 18:34:19 +0530

    Update __init__.py

--- project1/Grantha/__init__.py
+++ project1/Grantha/__init__.py
@@ -1,9 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-
+from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
-
 
 app = Flask(__name__)
 Bootstrap(app)
@@ -11,5 +10,5 @@ bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'PFzS9qWgQqTUv6BQjQlmJg'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Database.db'
 db = SQLAlchemy(app)
-
+login_manager = LoginManager(app)
 from Grantha import routes
