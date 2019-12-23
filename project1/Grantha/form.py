e59a272dbe9586687f8e573771a8e9696fc47a6c
Y-KUN-21 <57254049+Y-KUN-21@users.noreply.github.com>
2019-12-23 18:33:24 +0530

    Update form.py

--- project1/Grantha/form.py
+++ project1/Grantha/form.py
@@ -1,20 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length,Email,Required
+from Grantha.models import User
+from wtforms.validators import DataRequired, Length, Email, ValidationError
 
 
 class signupForm(FlaskForm):
-    username = StringField('Username',validators=[DataRequired(),Length(min=2,max=20)])
-    email_id = StringField('Email-Address',validators=[DataRequired(),Email()])
-    password =PasswordField('Password',validators=[DataRequired(),Length(min=8)])
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    email_id = StringField('Email-Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired(), Length(min=8)])
     submit = SubmitField('Sign up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('Username is already taken.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email_id=email.data).first()
+        if user:
+            raise ValidationError('Email is already taken.')
 
 
 class loginForm(FlaskForm):
-    email_id = StringField('Email-Address',validators=[DataRequired(),Email()])
-    password =PasswordField('Password',validators=[DataRequired()])
+    email_id = StringField('Email-Address', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
     submit = SubmitField('Login')
-
 
 
